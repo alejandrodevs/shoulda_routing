b9b6a84e14e10621bc02c71b9f226ae50a84b908
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2016-03-30 15:25:05 -0600

    Updating spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-require 'shoulda_routing'
-require 'rails/all'
-
 require 'coveralls'
 Coveralls.wear!
-Coveralls::Output.silent = true
 
 require 'simplecov'
-SimpleCov.start do
-  minimum_coverage 85
-end
+SimpleCov.start
+
+require 'shoulda_routing'
+require 'rails/all'
